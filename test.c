a22f20a679871e0677e68f8f5e370ca5a42dad60
Martin Haan <mhaan@f0r4s10.codam.nl>
2023-02-06 12:19:22 +0100

    made error handling

---
+++ test.c
@@ -0,0 +1,20 @@
+#include	<stdio.h>
+#include	"./libft/libft.h"
+#include	"push_swap.h"
+
+
+int	main (int argc, char *argv[])
+{
+	check_error(argc, argv);
+
+	// int i = 1;
+	// (void)argc;
+
+	// while (argv[i])
+	// {
+	// 	printf("FT: %i\n", ft_atoi(argv[i]));
+	// 	printf("OG: %i\n", atoi(argv[i]));
+	// 	// printf("Me: %i\n", ps_atoi(argv[i]));
+	// 	i++;
+	// }
+}
